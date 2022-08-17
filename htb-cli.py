36c745c762d3f585ae57ddc098d5154c13778015
nayr <ryan.syed1305@gmail.com>
2022-08-17 14:04:13 -0500

    Refined current build
    Added verbosity, handled entity ids properly, fixed downloads and credential stuff

--- htb-cli.py
+++ htb-cli.py
@@ -5,26 +5,25 @@ import json
 import time
 from hackthebox import *
 from utils.colors import *
-from os.path import expanduser
+from os.path import expanduser, isdir, isfile, exists
 
 # TODO: Fortresses, Endgames, and VPN (maybe HBG?)
 # TODO: Add option to stop and/or reset container/machine. Not entirely sure 
 #       how to accomplish this when we lose the handle after execution but we'll
 #       see
-# TODO: If a machine/challenge id is supplied, convert it to the name and handle that 
-#       correctly
+# TODO: Improve aesthetics of verbosity
 
 def get_args():
     # Begin original commands - mostly related to authentication
     parser = argparse.ArgumentParser(description="Interact with HackTheBox from the command line.")
     parser.add_argument('-c', '--cache', type=str, help='Path to cached credentials (default: ~/.config/.htbcache)', default='~/.config/.htbcache')
-    parser.add_argument('-i', '--input-creds', action="store_true", help='Use stdin to input credentials')
+    parser.add_argument('-v', '--verbose', action="store_true", help="increase output verbosity")
     subparsers = parser.add_subparsers(title='subcommands', dest='subcommand')
 
     # Begin challenge subcommand
     parser_chall = subparsers.add_parser('challenge', help="Interact with challenges.")
     parser_chall.add_argument('-n', '--name', required=True, help='Name of the challenge, or the challenge ID.')
-    parser_chall.add_argument('-p', '--path', default='', type=str, help='Download challenge files to the specified path.')
+    parser_chall.add_argument('-p', '--path', type=str, help='Download challenge files to the specified path.', default=None)
     parser_chall.add_argument('-s', '--start-docker', action="store_true", help='Start Docker instance.')
     parser_chall.add_argument('-f', '--flag', type=str, help='Submit flag.')
     parser_chall.add_argument('-d', '--difficulty', type=int, choices=range(10,101), metavar="[10-100]", help='Submit difficulty rating, 10-100.')
@@ -36,6 +35,7 @@ def get_args():
     parser_mach.add_argument('--release-arena', action="store_true", help='Spawn machine in release arena.')
     parser_mach.add_argument('-f', '--flag', type=str, help='Submit flag.')
     parser_mach.add_argument('-d', '--difficulty', type=int, choices=range(10,101), metavar="[10-100]", help='Submit difficulty rating, 10-100.')
+    
     args = parser.parse_args()
 
     return args
@@ -43,26 +43,49 @@ def get_args():
 class HTBCLI:
     def __init__(self) -> None:
         self.args = get_args()
-        print(f"[ DEBUG ] args={self.args}")
+        if self.args.verbose:
+            #print(f"[ DEBUG ] args={self.args}")
+            self.print_args()
         self.subcommand = self.args.subcommand
+        
+        if self.args.name is not None:
+            if self.args.name.isdecimal:
+                self.args.name = int(self.args.name)
+
         self.cred_management()
- 
+
     def run(self):
+        """Executes the specified subcommand"""
         if self.subcommand == 'challenge':
             self.challenge()
         elif self.subcommand == 'machine':
             self.machine()
 
+    def print_args(self):
+        """Print passed arguments to be more verbose"""
+        print('HTB CLI - version v0.1')
+        print('author: @An00bRektn (an00brektn.github.io)')
+        print('─'*50)
+        for key,value in vars(self.args).items():
+            if value is not None: 
+                print(f"{key+' '*(15-len(key))} │ {value}")
+        print('─'*50)
+
     def cred_management(self):
         """Applies flags to authenticate with the API, either through input or caching."""
-        if self.args.input_creds:
-            self.username = input('Email: ')
-            self.password = getpass.getpass('Password: ')
+        if exists(expanduser(self.args.cache)) == False:
+            self.username = input(recc + 'Email: ')
+            self.password = getpass.getpass(recc + 'Password: ')
             try:
                 self.client = HTBClient(email=self.username, password=self.password)
             except errors.ApiError as e:
                 print(printError + f"Couldn't authenticate: {e}")
                 print(info + "Exiting...")
+                exit()
+            except AttributeError as e:
+                print(printError + f"Couldn't authenticate: {e}")
+                print(info + "Exiting...")
+                exit()
         else:
             try:
                 self.client = HTBClient(cache=expanduser(self.args.cache))
@@ -76,25 +99,27 @@ class HTBCLI:
     def challenge(self):
         """Facilitates interactions with the challenges. TODO: Move to separate class/file"""
         # Attempt to access the challenge to return a Challenge object
-        print(info + f'Accessing {self.args.name}...')
+        print(info + f'Accessing challenge {self.args.name}...')
         try:
             chall = self.client.get_challenge(self.args.name)
         except errors.NotFoundException:
             print(printError + "Could not find challenge. Exiting...")
             exit()
+        print(good + f"Challenge {chall.name} ({chall.category}) retrieved!")
 
         # If the user has specified they want to download the files, download the files
-        if self.args.path != '' and chall.has_download:
-            print(info + f'Downloading challenge files to {self.args.path}')
+        if self.args.path is not None and chall.has_download:
+            path = expanduser(self.args.path)
+            print(info + f'Downloading challenge files to {path}')
             try:
-                chall.download(self.args.path)
-                print(good + "Download successful!")
-            # TODO: Implement a check before downloading the file to see if it's a directory or not.
-            #       A 30s lockout is just too brutal.
-            except IsADirectoryError:
-                time.sleep(self.client.challenge_cooldown - int(time.time()))
-                chall.download(self.args.path.rstrip() + f'/{chall.name}.zip')
-                print(good + "Download successful!")
+                if isdir(path):
+                    chall.download(self.args.path.rstrip() + f'/{chall.name}.zip')
+                    print(good + "Download successful!")
+                elif isfile(path):
+                    chall.download(self.args.path)
+                    print(good + "Download successful!")
+                else:
+                    print(important + "Path specified is not a directory or file, skipping...")
             except Exception as e:
                 print(printError + f"We encountered an error: {e}")
 
@@ -124,13 +149,15 @@ class HTBCLI:
     def machine(self):
         """Facilitates interactions with the machines. TODO: Move to separate class/file"""
         # Pull down the machine object
-        print(info + f'Accessing {self.args.name}...')
+        print(info + f'Accessing machine {self.args.name}...')
         try:
             machine = self.client.get_machine(self.args.name)
             name = machine.name
         except errors.NotFoundException:
             print(printError + "Could not find machine. Exiting...")
             exit()
+
+        print(good + f"Machine {machine.name} ({machine.difficulty}) retrieved!")
 
         # attempt to spawn the machine either normally or in release arena
         # TODO: Known Issue, after talking with clubby, found out that there
@@ -165,8 +192,6 @@ class HTBCLI:
                 print(printError + f"We encountered an error: {e}")
         elif (self.args.flag is None) != (self.args.difficulty is None):
             print(important + "You need a flag and a difficulty to submit!")
-
-        
 
 if __name__ == "__main__":
     h = HTBCLI()
